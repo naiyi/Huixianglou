0a9decd2da3fb4efd0a58b0211b0af14cfeb2149
liubin <liubinjaywln@163.com>
2014-06-11 08:43:34 +0800

    菜品类

---
+++ HXL/Model/JDDishModel.m
@@ -0,0 +1,13 @@
+//
+//  JDDishModel.m
+//  HXL
+//
+//  Created by 刘斌 on 14-6-10.
+//  Copyright (c) 2014年 胶东在线. All rights reserved.
+//
+
+#import "JDDishModel.h"
+
+@implementation JDDishModel
+
+@end
