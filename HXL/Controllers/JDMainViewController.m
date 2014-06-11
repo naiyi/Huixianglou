054323e6d4181997ef1b2742f20b7a2aa98e8437
Roc <cuipengxiang@gmail.com>
2014-06-11 08:08:43 +0800

    添加判断iOS版本的代码

--- HXL/Controllers/JDMainViewController.m
+++ HXL/Controllers/JDMainViewController.m
@@ -32,16 +32,16 @@
     [self setNavigationLeftButtonWithImage:[UIImage imageNamed:@"user_btn_bg"] Target:self Action:@selector(onUserButtonClicked)];
     [self setNavigationRightButtonWithImage:[UIImage imageNamed:@"setting_btn_bg"] Target:self Action:@selector(onSettingButtonClicked)];
     
-    centerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0, 60.0, 320.0, 275.0)];
+    centerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0, IS_iOS7 ? Nav_Height - 4.0 : - 4.0, 320.0, App_Height - 96.0 - 49.0 - Nav_Height + 4.0)];
     [centerScrollView setBackgroundColor:[UIColor redColor]];
     [self.view addSubview:centerScrollView];
     
-    addrAndTelView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 335.0, 320.0, 96.0)];
+    addrAndTelView = [[UIView alloc] initWithFrame:CGRectMake(0.0, IS_iOS7 ? centerScrollView.frame.size.height + Nav_Height - 4.0 : centerScrollView.frame.size.height - 4.0, 320.0, 96.0)];
     [addrAndTelView setBackgroundColor:[UIColor colorWithRed:1.0 green:0.980 blue:0.906 alpha:1.0]];
     [self.view addSubview:addrAndTelView];
     [self setupAddrAndTelView];
     
-    bottomView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 431.0, 320.0, 49.0)];
+    bottomView = [[UIView alloc] initWithFrame:CGRectMake(0.0, addrAndTelView.frame.origin.y + 96.0, 320.0, 49.0)];
     [bottomView setBackgroundColor:[UIColor colorWithRed:0.941 green:0.941 blue:0.941 alpha:1.0]];
     [self.view addSubview:bottomView];
     [self setupBottomView];
